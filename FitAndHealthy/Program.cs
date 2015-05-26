c399971f15a4a7460355bb3e7dead5a8f650ea0f
lejlaa <lfazlagic1@etf.unsa.ba>
2015-05-26 18:36:38 +0200

    commit 1
    some classes

---
+++ FitAndHealthy/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitAndHealthy
+{
+    public class Program
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Duration { get; set; }
+        public int Rating { get; set; }
+        public int RatedByNo { get; set;}
+        public string VideoLink { get; set; }
+
+        //public virtual Diet Diet { get; set; }
+
+    }
+}
