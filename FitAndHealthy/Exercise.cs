28720121bc4cde714a64dadc290aa50bfb7a6178
Nedim <nb15321@etf.unsa.ba>
2015-05-27 20:40:28 +0200

    added enities

--- FitAndHealthy/Exercise.cs
+++ FitAndHealthy/Exercise.cs
@@ -6,19 +6,17 @@ using System.Threading.Tasks;
 
 namespace FitAndHealthy
 {
-    public class Program
+    class Exercise
     {
-        public Program()
-        {
-            this.Trainings = new List<Training>();
-        }
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public double Duration { get; set; }
+        public String Name { get; set; }
         public int Rating { get; set; }
         public int RatedByNo { get; set; }
-        public string VideoLink { get; set; }
+        public String Video { get; set; }
+
+        public String Image { get; set; }
+
+        public DateTimeOffset Duration { get; set; }
 
         public virtual ICollection<Training> Trainings { get; set; }
 
