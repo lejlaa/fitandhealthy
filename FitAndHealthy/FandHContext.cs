f062e2d15eaf4de8ce45a2e20a680ca9dd55475c
bmuratovic1 <bmuratovic@etf.unsa.ba>
2015-05-30 15:03:15 +0200

    dbcontext

--- FitAndHealthy/FandHContext.cs
+++ FitAndHealthy/FandHContext.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.Entity;
+using FitAndHealthy.Mappers;
 
 namespace FitAndHealthy
 {
@@ -27,5 +28,32 @@ namespace FitAndHealthy
             //Database.SetInitializer<FandHContext>(new FandHDbInitializer());
             //Database.SetInitializer<FandHContext>(null);
         }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new DietMap()); 
+            modelBuilder.Configurations.Add(new ExerciseMap()); 
+            modelBuilder.Configurations.Add(new ActionMap()); 
+            modelBuilder.Configurations.Add(new ProgramMap()); 
+            modelBuilder.Configurations.Add(new RoleMap()); 
+            modelBuilder.Configurations.Add(new UserMap());
+            modelBuilder.Configurations.Add(new TrainingMap());
+            modelBuilder.Configurations.Add(new CommentMap());
+            modelBuilder.Configurations.Add(new CategoryMap());
+
+            modelBuilder.Entity<Program>().HasMany(u => u.Comments).WithMany();
+
+
+ 
+
+            modelBuilder.Entity<Program>().HasMany<Training>(x => x.Trainings).WithMany(p => p.Programs).Map(m => m.ToTable("ProgramTraining").MapLeftKey("ProgramId").MapRightKey("TrainingId")); 
+            
+            
+            //modelBuilder.Entity<Day>().HasRequired(x => x.Person).WithMany(p => p.Days).Map(m => m.MapKey("Person")); 
+            //modelBuilder.Entity<Detail>().HasRequired(x => x.Day).WithMany(p => p.Details).Map(m => m.MapKey("Day")); 
+            //modelBuilder.Entity<Detail>().HasRequired(x => x.Project).WithMany(p => p.Details).Map(m => m.MapKey("Project"));
+            
+            base.OnModelCreating(modelBuilder);
+        } 
     }
 }
