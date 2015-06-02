8bfc05d21c857bdddca14de333df0886ad358b30
lejlaa <lfazlagic1@etf.unsa.ba>
2015-06-02 22:05:18 +0200

    API zapocet

---
+++ FitAndHealthy/IRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitAndHealthy
+{
+    public interface IRepository
+    {
+
+        //IQueryable<Program> GetAllPrograms();
+        //IQueryable<Training> GetAllTrainings();
+
+
+        /*
+        Person GetPerson(int id);
+        IQueryable<Person> GetAllPersons();
+        double GetAllWorkHoursByMonthAndYear(int year, int month);
+        IQueryable<Detail> GetAllDetailsByMonthAndYearForOnePerson(int year, int month);
+        int GetNumberOfPTODaysInMonth(int year, int month);
+        IQueryable<Team> GetAllTeams();
+        IQueryable<ApiUser> GetApiUsers();
+        AuthToken GetAuthToken(string token);
+        bool Insert(AuthToken token);
+         */
+    }
+}
---
+++ FitAndHealthy/Repository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitAndHealthy
+{
+    public class Repository : IRepository
+    {
+        /*
+        protected FandHContext ctx;
+
+        public Repository(FandHContext dataContext)
+        {
+            ctx = dataContext;
+        }
+
+        public FandHContext dataContext()
+        {
+            return ctx;
+        }
+        
+        public IQueryable<Program> GetAllPrograms
+        {
+            return ctx.Programs.OrderBy(x => x.Name); 
+            
+        }
+         */
+    }
+}
